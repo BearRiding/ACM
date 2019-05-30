ed363d9c0a1aea38ad18da6547afabe74b17c85f
BearKing <BearKing@top.com>
2019-05-30 09:02:20 +0800

    fist

---
+++ test/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    cout << "hello world";
+    vector<int> lala;
+    lala.push_back(12);
+    cout << lala[0];
+    getchar();
+    return 0;
+}
